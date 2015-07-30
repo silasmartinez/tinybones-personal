847be42f0c1dcb438883a1d48ec63b8243d2dbb1
silasmartinez <silasm@gmail.com>
2015-07-30 15:00:46 -0600

    Enabled multiuser..

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var faith = require('../lib/faith')
 var axios = require('axios')
 
 /* GET home page. */
@@ -11,16 +10,5 @@ router.get('/', function (req, res, next) {
 router.get('/styleguide', function (req, res, next) {
   res.render('styleguide');
 });
-
-router.get('/faith', function (req, res, next) {
-  var testOfFaith = new faith;
-  console.log(testOfFaith)
-  testOfFaith.believe('silas', axios.get('https://api.github.com/users/silasmartinez/repos') )
-  testOfFaith.believe('akyuna', axios.get('https://api.github.com/users/AkyunaAkish/repos') )
-  testOfFaith.manifest().then(function (obj) {
-    console.log(obj.silas)
-  });
-  res.redirect('/');
-})
 
 module.exports = router;
