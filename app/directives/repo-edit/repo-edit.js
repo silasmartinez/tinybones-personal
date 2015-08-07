3864fffd864a99e2289c5eb38b4c33370687cda1
silasmartinez <silasm@gmail.com>
2015-08-07 14:04:28 -0600

    Refining angular

---
+++ app/directives/repo-edit/repo-edit.js
@@ -0,0 +1,12 @@
+/**
+ * Created by silasmartinez on 8/3/15.
+ */
+export default ngModule => {
+  ngModule.directive('repoEdit', () => {
+    require('./repo-edit.css');
+    return {
+      restrict: 'E',
+      template: require('./repo-edit.html')
+    };
+  });
+};
